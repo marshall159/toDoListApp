d7ec40cdaf70bf4393373a39995cabbee1fa6165
Aneel Marshall <aneel.marshall@outlook.com>
2017-05-17 16:47:37 +0100

    Complete Version 2

--- toDoListApp.js
+++ toDoListApp.js
@@ -14,3 +14,25 @@ todos[0] = 'item 1 updated';
 // It should have a way to delete a todo
 todos.splice(0, 1);
 
+// It should have a function to display todos
+function displayTodos() {
+	console.log('My Todos: ', todos);
+}
+
+// It should have a function to add todos
+function addTodo(task) {
+	todos.push(task);
+	displayTodos();
+}
+
+// It should have a function to change todos
+function changeTodo(position, newValue) {
+	todos[position] = newValue;
+	displayTodos();
+}
+
+// It should have a function to delete todos
+function deleteTodo(position) {
+	todos.splice(position, 1);
+	displayTodos();
+}
