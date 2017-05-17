048490ffc7edb67923ec3c2c1773d79e54cf4e31
Aneel Marshall <aneel.marshall@outlook.com>
2017-05-17 15:50:22 +0100

    Complete Version 1

--- toDoListApp.js
+++ toDoListApp.js
@@ -1,3 +1,16 @@
+
+// It should have a place to store todos
 var todos = ['item 1', 'item 2', 'item 3'];
 
-console.log('My Todos: ' + todos); 
+// It should have a way to display todos
+console.log('My Todos: ', todos); 
+
+// It should have a way to add new todos
+todos.push('item 4');
+
+// It should have a way to change a todo
+todos[0] = 'item 1 updated';
+
+// It should have a way to delete a todo
+todos.splice(0, 1);
+
