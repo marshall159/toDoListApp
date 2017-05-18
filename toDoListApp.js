92d36ffb650c3585929418d832a8b3fe09668c9b
Aneel Marshall <aneel.marshall@outlook.com>
2017-05-18 23:17:20 +0100

    Complete Version 6: Toggle all todos method

--- toDoListApp.js
+++ toDoListApp.js
@@ -12,6 +12,9 @@
 // .displayTodos should tell you if .todos is empty
 // .displayTodos should show .completed
 
+// .toggleAll: If everything is true, make everything false
+// .toggleAll: Otherwise, make everything true
+
 var todoList = {
 	todos: [],
 	displayTodos: function() {
@@ -48,6 +51,32 @@ var todoList = {
 	toggleCompleted: function(position) {
 		var todo = this.todos[position];
 		todo.completed = !todo.completed;
+		this.displayTodos();
+	},
+	toggleAll: function() {
+		var totalTodos = this.todos.length;
+		var completedTodos = 0;
+
+		//Get number of completed todos
+		for (var i = 0; i < totalTodos; i++) {
+  			if (this.todos[i].completed === true) {
+  				completedTodos++;
+  			}
+		}
+
+		// Case 1: If everything is true, make everything false
+		if (completedTodos === totalTodos) {
+			for (var i = 0; i < totalTodos; i++) {
+				this.todos[i].completed = false;
+			}
+		}
+
+		// Case 2: Otherwise make everything true
+		else {
+			for (var i = 0; i < totalTodos; i++) {
+				this.todos[i].completed = true;
+			}
+		}
 		this.displayTodos();
 	}
 };
