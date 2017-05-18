8f8eabbc82b69346d858831ad73db80ed5c42658
Aneel Marshall <aneel.marshall@outlook.com>
2017-05-18 14:10:30 +0100

    Complete Version 5: Display todos and visible completed status

--- toDoListApp.js
+++ toDoListApp.js
@@ -8,10 +8,27 @@
 // todoList.changeTodo should change the todoText property
 // todoList.toggleCompleted should change the completed property
 
+// .displayTodos should show .todoText
+// .displayTodos should tell you if .todos is empty
+// .displayTodos should show .completed
+
 var todoList = {
 	todos: [],
 	displayTodos: function() {
-		console.log('My Todos: ', this.todos);
+		if (this.todos.length === 0) {
+			console.log('Your todo list is empty!');
+		}
+		else {
+			console.log('My Todos:')
+			for (var i = 0; i < this.todos.length; i++) {
+				if (this.todos[i].completed === true) {
+					console.log('(x)', this.todos[i].todoText);
+				}
+				else {
+					console.log('( )', this.todos[i].todoText);
+				}				
+			}
+		}				
 	},
 	addTodo: function(todoText) {
 		this.todos.push({
